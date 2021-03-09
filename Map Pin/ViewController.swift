6698110c1a457059ae6f96eefd02bfeed245fc3a
JiHoonAHN <ahnjh1028@naver.com>
2021-03-09 23:26:38 +0900

    Map pin

--- Map Pin/ViewController.swift
+++ Map Pin/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365)
+        annotation.title = "The White house"
+        annotation.subtitle = "president house"
+        mapView.addAnnotation(annotation)
+        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
     }
 
 
